ef481044d0845f43c2dddde5b58e95543e9b522e
hsiaokang <hsiao52hz@163.com>
2017-10-13 17:53:11 +0800

    瀑布流布局

---
+++ waterfull/scripts/main.js
@@ -0,0 +1,124 @@
+window.onload = function () {
+    // 布局
+    waterfull('main', 'box');
+    // 数据加载
+    var data = {obj:[{src:'0.jpg'},{src:'0.jpg'},{src:'0.jpg'},{src:'0.jpg'},{src:'0.jpg'},{src:'0.jpg'}]}
+    window.onscroll = function(){
+        if(checkScrollSlide()){
+            // 模拟
+            var oParent = document.getElementById('main');
+
+            for(var i=0; i<data.obj.length; i++){
+                var oBox = document.createElement('div');
+                oBox.className = 'box';
+                oParent.appendChild(oBox);
+
+                var oPic = document.createElement('div');
+                oPic.className = 'pic';
+                oBox.appendChild(oPic);
+
+                var oImg = document.createElement('img');
+                oImg.src = './images/'+data.obj[i].src;
+                oPic.appendChild(oImg);
+            }
+
+            waterfull('main', 'box');
+        }
+        
+    }
+}
+window.onresize=function(){
+    waterfull('main', 'box');
+}
+function waterfull(parent, box) {
+    // 将parent下所有的 class为 box的元素取出来;
+    var oParent = document.getElementById(parent);
+    // ie11 以下不支持
+    // var oBox = oParent.querySelectorAll(box);
+
+    var oBoxs = getByClass(oParent, box);
+    console.log(oBoxs);
+
+    //计算页面的列数
+    var oBoxW = oBoxs[0].offsetWidth;
+    var pageW = document.documentElement.clientWidth;
+    var clos = Math.floor(pageW / oBoxW)
+    console.log('列数',clos)
+
+    // 设置main的宽度
+    oParent.style.cssText = 'width:' + oBoxW * clos + "px";
+
+    var hArr = [];  // 存储高度
+    for (var i = 0; i < oBoxs.length; i++) {
+        if(i<clos){
+            console.log(oBoxs[i]);
+            console.log(oBoxs[i].offsetHeight)
+            hArr.push(oBoxs[i].offsetHeight);
+
+    
+        }else{
+
+
+            var minH = Math.min.apply(null,hArr);
+            // ie9以下不支持indexOf
+            // var minInx = hArr.indexOf(minH);
+            
+            var minInx  = getMinIndex(hArr,minH);
+            
+            console.log('min',minInx);
+
+            // 设置位置
+            oBoxs[i].style.position = 'absolute';
+            oBoxs[i].style.top=minH+'px';
+            oBoxs[i].style.left = oBoxW*minInx+'px';
+            
+            hArr[minInx] += oBoxs[i].offsetHeight;
+    console.log('arr',hArr)
+    
+        }
+    
+    }
+    
+}
+
+//  将parent下所有的 class 元素取出来
+function getByClass(parent, classN) {
+    // 取出所有，遍历 判断
+    var startT = new Date().getTime();
+    var oBox = [];  // 存储 box
+    var oElements = parent.getElementsByTagName('*');
+
+    for (var i = 0; i < oElements.length; i++) {
+        if (oElements[i].className == classN) {
+            oBox.push(oElements[i])
+        }
+    }
+    var endT = new Date().getTime()
+    console.log('元素get时间', endT - startT + ' ms')
+    return oBox;
+
+}
+
+// 取得数组中值的索引
+function getMinIndex(arr,num){
+    for(var i=0;i<arr.length; i++){
+        if(arr[i] == num){
+            return i
+        }
+    }
+}
+
+// 检测是否具备加载数据条件
+// 当已加载块的最后一个自身露出屏幕一半时加载下一段数据
+function checkScrollSlide(){
+    var oParent = document.getElementById('main');
+    var oBox = getByClass(oParent,'box');
+
+    // 距离顶部的距离+自身一半高
+    var lastBoxH = oBox[oBox.length-1].offsetTop + Math.floor(oBox[oBox.length-1].offsetHeight/2);
+    // 考虑混杂模式和标准模式
+    var scrollT = document.body.scrollTop || document.documentElement.scrollTop;
+    var windowH = document.body.clientHeight || document.documentElement.clientHeight;
+
+    return lastBoxH<scrollT+windowH?true:false
+}
